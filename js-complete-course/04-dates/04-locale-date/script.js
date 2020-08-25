b6ef8547c6a7b9154232162b8465a020ba510cf8
MZ <nikita.zholkovskyi@gmail.com>
2020-08-25 16:59:06 +0200

    locale date

--- js-complete-course/04-dates/04-locale-date/script.js
+++ js-complete-course/04-dates/04-locale-date/script.js
@@ -9,10 +9,46 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+    var today = new Date();
+    var months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+    var monthIndex = today.getMonth();
+    var currentmonth = months[monthIndex]
+    var currentyear = today.getFullYear();
+    var currentdate = today.getDate();
+    var days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ]
+    var dayIndex = today.getDay()
+    var currentday = days[dayIndex]
+    var currenthour = today.getHours();
+    var currentmin = today.getMinutes()
 
+    // var monthIndex = d.getMonth()
+    // var monthName = months[monthIndex]
+
+    var formatted = `${currentday} ${currentdate} ${currentmonth} ${currentyear}, ${currenthour}h${currentmin}`
+    document.getElementById("target").innerHTML = formatted
     // your code here
 
 })();
