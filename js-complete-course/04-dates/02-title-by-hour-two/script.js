89c321f8e92a3787185b364be32b085a8f6a62d3
MZ <nikita.zholkovskyi@gmail.com>
2020-08-25 14:19:44 +0200

    title by hour two

--- js-complete-course/04-dates/02-title-by-hour-two/script.js
+++ js-complete-course/04-dates/02-title-by-hour-two/script.js
@@ -9,10 +9,16 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-
-    // your code here
+    var myDate = new Date();
+    var hrs = myDate.getHours();
+    var min = myDate.getMinutes();
+    var greet;
+    if (hrs <= 17 && min <= 30)
+        greet = "Hello!";
+    else
+        greet = "Good Evening!";
+    document.getElementById("target").innerHTML = greet
 
 })();
