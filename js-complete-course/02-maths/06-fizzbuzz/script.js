db2d99b3c171861d13c8c601dcbb9fb68fbc4af2
MZ <nikita.zholkovskyi@gmail.com>
2020-08-25 10:56:20 +0200

    fizzbuzz

--- js-complete-course/02-maths/06-fizzbuzz/script.js
+++ js-complete-course/02-maths/06-fizzbuzz/script.js
@@ -9,8 +9,20 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    for (var x = 1; x <= 100; x++) {
+        if ((x % 3 == 0) && (x % 5 == 0)) {
+            console.log(x + " fizzbuzz");
+        } else if (x % 3 == 0) {
+            console.log(x + " fizz");
+        } else if (x % 5 == 0) {
+            console.log(x + " buzz");
+        } else {
+            {
+                console.log(x);
+            }
 
+        }
+    }
 })();
