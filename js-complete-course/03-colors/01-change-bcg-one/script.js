785b91e8c0d75374d1b5bb47d546a08ff5c2a5d9
MZ <nikita.zholkovskyi@gmail.com>
2020-08-25 11:42:11 +0200

    changed bg color

--- js-complete-course/03-colors/01-change-bcg-one/script.js
+++ js-complete-course/03-colors/01-change-bcg-one/script.js
@@ -9,8 +9,19 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
-    // your code here
+(function () {
+    var bg = document.getElementById('bg');
+    red.addEventListener('click', function () {
+        bg.style.backgroundColor = "red";
+    });
+    green.addEventListener('click', function () {
+        bg.style.backgroundColor = "green";
+    });
+    yellow.addEventListener('click', function () {
+        bg.style.backgroundColor = "yellow";
+    });
+    blue.addEventListener('click', function () {
+        bg.style.backgroundColor = "blue";
+    });
 
 })();
