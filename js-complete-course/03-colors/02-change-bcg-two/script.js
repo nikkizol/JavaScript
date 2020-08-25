e786ed205d7a3e88fd52e06aa3166cab29a7a5fe
MZ <nikita.zholkovskyi@gmail.com>
2020-08-25 11:55:03 +0200

    change bcg two

--- js-complete-course/03-colors/02-change-bcg-two/script.js
+++ js-complete-course/03-colors/02-change-bcg-two/script.js
@@ -9,8 +9,12 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+    run.addEventListener('click', function () {
+        document.body.style.background = document.getElementById('color').value;
+    });
 
-    // your code here
 
 })();
+
+
