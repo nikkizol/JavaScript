bdc66e855543bf5ef175563ac6d26cf335964e82
MZ <nikita.zholkovskyi@gmail.com>
2020-08-26 10:17:06 +0200

    get spooky fridays

--- js-complete-course/04-dates/05-get-spooky-fridays/script.js
+++ js-complete-course/04-dates/05-get-spooky-fridays/script.js
@@ -9,8 +9,22 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+    document.getElementById('run').addEventListener('click', function (e) {
+        var year = parseInt(document.getElementById('year').value);
 
-    // your code here
+        function arrayOfFridaythe13thsIn() {
+            var array = [];
+            for (var month = 0; month < 12; month++) {
+                var d = new Date(year, month, 13);
+                if (d.getDay() == 5) {
+                    array.push(d);
+                }
+            }
+            return array;
+        }
 
+        alert(arrayOfFridaythe13thsIn().toLocaleString(undefined, {day: "numeric", month: "long"}));
+    });
 })();
+
