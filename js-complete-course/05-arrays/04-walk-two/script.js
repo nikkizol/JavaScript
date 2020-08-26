d2cd90e96dd05bb17d7039c6563ffffc54ec1499
MZ <nikita.zholkovskyi@gmail.com>
2020-08-26 11:47:56 +0200

    walk two

--- js-complete-course/05-arrays/04-walk-two/script.js
+++ js-complete-course/05-arrays/04-walk-two/script.js
@@ -9,23 +9,25 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    var fruits = [
-        "pomme",
-        "poire",
-        "fraise",
-        "tomate",
-        "kiwi",
-        "banane",
-        "orange",
-        "mandarine",
-        "durian",
-        "pêche",
-        "raisin",
-        "cerise",
-    ];
-
-    // your code here
-
+    document.getElementById('run').addEventListener('click', function (e) {
+        var fruits = [
+            "apple",
+            "pear",
+            "raspberry",
+            "tomatos",
+            "kiwi",
+            "banana",
+            "orange",
+            "mandarin",
+            "durian",
+            "peache",
+            "grap",
+            "cherry",
+        ];
+        fruits.forEach(function (entry) {
+            console.log(entry);
+        });
+    });
 })();
