c00ec3c9bd1162fb37efc89a2c1c50c9b4660319
MZ <nikita.zholkovskyi@gmail.com>
2020-08-26 10:23:59 +0200

    get element

--- js-complete-course/05-arrays/01-get-element/script.js
+++ js-complete-course/05-arrays/01-get-element/script.js
@@ -9,23 +9,23 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+    document.getElementById('run').addEventListener('click', function (e) {
+        var fruits = [
+            "apple",
+            "pear",
+            "raspberry",
+            "tomatos",
+            "kiwi",
+            "banana",
+            "orange",
+            "mandarin",
+            "durian",
+            "peache",
+            "grap",
+            "cherry",
+        ];
 
-    var fruits = [
-        "apple",
-        "pear",
-        "raspberry",
-        "tomatos",
-        "kiwi",
-        "banana",
-        "orange",
-        "mandarin",
-        "durian",
-        "peache",
-        "grap",
-        "cherry",
-    ];
-
-    // your code here
-
+        alert(fruits [3])
+    });
 })();
