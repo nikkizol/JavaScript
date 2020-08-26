2f9869fe149cc45ac01369aee7070f521af8977a
MZ <nikita.zholkovskyi@gmail.com>
2020-08-26 11:38:17 +0200

    wlak one

--- js-complete-course/05-arrays/03-walk-one/script.js
+++ js-complete-course/05-arrays/03-walk-one/script.js
@@ -9,23 +9,25 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+    document.getElementById('run').addEventListener('click', function (e) {
+        var fruits = [
+            "apple",
+            "pear",
+            "raspberry",
+            "tomatos",
+            "kiwi",
+            "banana",
+            "orange",
+            "mandarin",
+            "durian",
+            "peache",
+            "grap",
+            "cherry",
+        ];
 
-    var fruits = [
-        "apple",
-        "pear",
-        "raspberry",
-        "tomatos",
-        "kiwi",
-        "banana",
-        "orange",
-        "mandarin",
-        "durian",
-        "peache",
-        "grap",
-        "cherry",
-    ];
-
-    // your code here
-
+        for (var i = 0; i < fruits.length; i++) {
+            console.log(fruits[i]);
+        }
+    });
 })();
