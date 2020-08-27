62e45b6b195be450239d74d57779fba9ca91b43b
MZ <nikita.zholkovskyi@gmail.com>
2020-08-27 10:46:59 +0200

    reduce array

--- js-complete-course/05-arrays/10-reduce-array/script.js
+++ js-complete-course/05-arrays/10-reduce-array/script.js
@@ -10,85 +10,87 @@
 // You will have time to focus on it later.
 
 (function() {
+    document.getElementById('run').addEventListener('click', function (e) {
+        var people = [
+            {
+                firstname: "Grenville",
+                lastname: "Stive",
+                age: 64,
+            },
+            {
+                firstname: "Luis",
+                lastname: "Jedrzejewsky",
+                age: 73,
+            },
+            {
+                firstname: "Teodor",
+                lastname: "Warrington",
+                age: 56,
+            },
+            {
+                firstname: "Danya",
+                lastname: "Bigby",
+                age: 17,
+            },
+            {
+                firstname: "Jemmy",
+                lastname: "Goodbur",
+                age: 87,
+            },
+            {
+                firstname: "Vilhelmina",
+                lastname: "Fost",
+                age: 43,
+            },
+            {
+                firstname: "Igor",
+                lastname: "Putson",
+                age: 45,
+            },
+            {
+                firstname: "Klement",
+                lastname: "Braybrook",
+                age: 88,
+            },
+            {
+                firstname: "Trefor",
+                lastname: "Le Jean",
+                age: 69,
+            },
+            {
+                firstname: "Tarrah",
+                lastname: "de Clerc",
+                age: 77,
+            },
+            {
+                firstname: "Brad",
+                lastname: "Hookes",
+                age: 82,
+            },
+            {
+                firstname: "Kippar",
+                lastname: "Fancet",
+                age: 25,
+            },
+            {
+                firstname: "Jecho",
+                lastname: "Hawket",
+                age: 87,
+            },
+            {
+                firstname: "Lief",
+                lastname: "Blazy",
+                age: 80,
+            },
+            {
+                firstname: "Jo-ann",
+                lastname: "Sacase",
+                age: 81,
+            },
+        ];
 
-    var people = [
-        {
-            firstname: "Grenville",
-            lastname: "Stive",
-            age: 64,
-        },
-        {
-            firstname: "Luis",
-            lastname: "Jedrzejewsky",
-            age: 73,
-        },
-        {
-            firstname: "Teodor",
-            lastname: "Warrington",
-            age: 56,
-        },
-        {
-            firstname: "Danya",
-            lastname: "Bigby",
-            age: 17,
-        },
-        {
-            firstname: "Jemmy",
-            lastname: "Goodbur",
-            age: 87,
-        },
-        {
-            firstname: "Vilhelmina",
-            lastname: "Fost",
-            age: 43,
-        },
-        {
-            firstname: "Igor",
-            lastname: "Putson",
-            age: 45,
-        },
-        {
-            firstname: "Klement",
-            lastname: "Braybrook",
-            age: 88,
-        },
-        {
-            firstname: "Trefor",
-            lastname: "Le Jean",
-            age: 69,
-        },
-        {
-            firstname: "Tarrah",
-            lastname: "de Clerc",
-            age: 77,
-        },
-        {
-            firstname: "Brad",
-            lastname: "Hookes",
-            age: 82,
-        },
-        {
-            firstname: "Kippar",
-            lastname: "Fancet",
-            age: 25,
-        },
-        {
-            firstname: "Jecho",
-            lastname: "Hawket",
-            age: 87,
-        },
-        {
-            firstname: "Lief",
-            lastname: "Blazy",
-            age: 80,
-        },
-        {
-            firstname: "Jo-ann",
-            lastname: "Sacase",
-            age: 81,
-        },
-    ];
-
-    // your code here
+        let total = people.reduce((a, b) => a + b.age, 0);
+        console.log(total)
+    });
 
 })();
