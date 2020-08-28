c244593e4558f6f8ebcc21402a8d940c69f42b56
MZ <nikita.zholkovskyi@gmail.com>
2020-08-28 11:49:09 +0200

    mini carousel

--- js-complete-course/06-dom/06-mini-carousel/script.js
+++ js-complete-course/06-dom/06-mini-carousel/script.js
@@ -9,16 +9,49 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+        let i = 1;
+        let gallery = [
+            "../../_shared/img/bell.svg",
+            "../../_shared/img/clock.svg",
+            "../../_shared/img/compass.svg",
+            "../../_shared/img/lemon.svg",
+            "../../_shared/img/map.svg",
+        ];
+        document.getElementById('next').addEventListener('click', function (e) {
+            document.querySelectorAll("img")[0].src = gallery[i];
+            i++;
+            if (i >= gallery.length) {
+                i = 0;
 
-    var gallery= [
-        "../../_shared/img/bell.svg",
-        "../../_shared/img/clock.svg",
-        "../../_shared/img/compass.svg",
-        "../../_shared/img/lemon.svg",
-        "../../_shared/img/map.svg",
-    ];
+            }
+        })
+    }
+)();
 
-    // your code here
 
-})();
+// let i=1
+//         for (var j = gallery.length; j--;) {
+//             var img = new Image();
+//             img.src = gallery[j];
+//         }
+//
+//         document.getElementById('next').addEventListener('click', function (e) {
+//             this.src = gallery[i >= gallery.length - 1 ? i = 0 : ++i];
+//         }, false);
+//
+//     }
+
+
+//let target = document.querySelectorAll("img")
+// let gallery = [
+//     "../../_shared/img/bell.svg",
+//     "../../_shared/img/clock.svg",
+//     "../../_shared/img/compass.svg",
+//     "../../_shared/img/lemon.svg",
+//     "../../_shared/img/map.svg",
+// ];
+// for (let i = 0; i < gallery.length; i++)
+// {
+//     document.getElementById('next').addEventListener('click', function (e) {
+//         target.src = gallery[i];
