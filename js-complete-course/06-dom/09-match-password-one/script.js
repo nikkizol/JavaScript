aa7d353355bc32aef183a22dc0e4eb907ca556a9
MZ <nikita.zholkovskyi@gmail.com>
2020-08-28 19:43:22 +0200

    match password one

--- js-complete-course/06-dom/09-match-password-one/script.js
+++ js-complete-course/06-dom/09-match-password-one/script.js
@@ -9,8 +9,23 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    function verif(e) {
+        var x = document.getElementById('pass-one').value
+        var y = document.getElementById('pass-two').value
+        if (x.match(/[a-z]/g) && x.match(
+            /[A-Z]/g) && x.match(
+            /[0-9]/g) && x.match(
+            /[^a-zA-Z\d]/g) && x.length >= 5 && x === y)
+            res = "TRUE";
+
+        else {
+            document.getElementById('pass-one').style.borderColor = "red"
+            document.getElementById('pass-two').style.borderColor = "red"
+        }
+    }
+
+    document.getElementById('run').addEventListener('click', verif)
 
 })();
