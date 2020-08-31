e933b5d036dc9bc45c9b8b199b3e879f09839bfc
MZ <nikita.zholkovskyi@gmail.com>
2020-08-31 11:21:22 +0200

    change event input two

--- js-complete-course/06-dom/12-change-event-input-two/script.js
+++ js-complete-course/06-dom/12-change-event-input-two/script.js
@@ -9,8 +9,17 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    document.getElementById('pass-one').onkeyup = function () {
+        let x = document.getElementById('pass-one').value
+        if (x.match(/[0-9]{2}/) && x.length >= 8)
+            document.getElementById("validity").innerHTML = "Ok"
+
+        else {
+            document.getElementById("validity").innerHTML = "Not ok"
+        }
+    }
+
 
 })();
