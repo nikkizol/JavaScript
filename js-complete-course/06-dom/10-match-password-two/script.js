2ee60234fb18374cd35fe163d17d52bd28f86cb7
MZ <nikita.zholkovskyi@gmail.com>
2020-08-31 09:28:12 +0200

    match password two

--- js-complete-course/06-dom/10-match-password-two/script.js
+++ js-complete-course/06-dom/10-match-password-two/script.js
@@ -9,8 +9,36 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+/* becode/javascript
+ *
+ * /06-dom/09-match-password-one/script.js - 6.9: vérification de mots de passe (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
 
-    // your code here
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function () {
+
+    function verif(e) {
+        var x = document.getElementById('pass-one').value
+        var y = document.getElementById('pass-two').value
+        if (x.match(/[a-z]/g) && x.match(
+            /[A-Z]/g) && x.match(
+            /[0-9]/g) && x.match(
+            /[^a-zA-Z\d]/g) && x.length >= 5 && x === y)
+            // res = "TRUE";
+        alert ('true');
+
+        else {
+            res = "error";
+            document.getElementById('pass-one').value = res;
+            document.getElementById('pass-two').value = res;
+        }
+    }
+
+    document.getElementById('run').addEventListener('click', verif)
 
 })();
