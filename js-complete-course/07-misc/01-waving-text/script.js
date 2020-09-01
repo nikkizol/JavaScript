f6989758d3ff784027c18d25adc23f3fcb76825c
MZ <nikita.zholkovskyi@gmail.com>
2020-09-01 19:03:46 +0200

    waving text added span

--- js-complete-course/07-misc/01-waving-text/script.js
+++ js-complete-course/07-misc/01-waving-text/script.js
@@ -9,8 +9,40 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+    let target = document.getElementById("target")
+    let targetArray = target.innerHTML.split(" ")
+    for (let i = 0; i < targetArray.length; i++) {
+        let span = document.createElement('span');
+        span.textContent = targetArray[i];
+        target.appendChild(span)
+        span.style.fontSize = "40px";
+        console.log(span)
 
-    // your code here
+
+        // let sp = document.querySelectorAll("span");
+        // sp[1].style.fontSize = "10px";
+
+
+    }
+
 
 })();
+
+// let target = document.getElementById("target").innerHTML
+// targetArray = target.split(" ")
+// // let span = target = "<span>"+targetArray[0]+"</span>"+ "<span>"+targetArray[1]+"</span>"+"<span>"+targetArray[2]+"</span>"+"<span>"+targetArray[3]+"</span>"+"<span>"+targetArray[4]+"</span>"+"<span>"+targetArray[5]+"</span>"+"<span>"+targetArray[6]+"</span>"+"<span>"+targetArray[7]+"</span>"+"<span>"+targetArray[8]+"</span>"+"<span>"+targetArray[9]+"</span>"+"<span>"+targetArray[10]+"</span>"+"<span>"+targetArray[11]+"</span>"+"<span>"+targetArray[12]+"</span>";
+// result = [];
+// for( let i = 0; i < targetArray.length; i++ ) {
+//     let span = '<span>' + targetArray[i] + '</span>';
+//     result.push(span);
+// }
+// let str = result.join(" ");
+// console.log(str)
+// let sp = document.querySelectorAll("span");
+// sp[0].style.fontSize = "4px";
+// document.getElementById("target").innerHTML = str;
+//
+//
+
+
