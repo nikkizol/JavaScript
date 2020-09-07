9291812718ec0b8fe509a74937317a2386218f59
MZ <nikita.zholkovskyi@gmail.com>
2020-09-07 10:14:59 +0200

    01 get posts

--- js-complete-course/08-asynchronous/01-get-posts/script.js
+++ js-complete-course/08-asynchronous/01-get-posts/script.js
@@ -10,5 +10,18 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  document.getElementById('run').addEventListener('click', function  () {
+      window.lib.getPosts(callback);
+
+  });
+  function callback (error, arr) {
+      if (error) {
+          console.error(error)
+      } else {
+          console.log(arr)
+      }
+
+  }
+
+
 })();
