1fb7225d3f870e38d7143d5047ac1de0761a3ba3
MZ <nikita.zholkovskyi@gmail.com>
2020-09-08 11:57:43 +0200

    09-fetch/01-list-to-console/

--- js-complete-course/09-fetch/01-list-to-console/script.js
+++ js-complete-course/09-fetch/01-list-to-console/script.js
@@ -10,5 +10,11 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById('run').addEventListener('click', function  () {
+        fetch('http://localhost:3000/heroes')
+            .then(response => response.json())
+            .then(data => console.log(data));
+
+    });
+
 })();
