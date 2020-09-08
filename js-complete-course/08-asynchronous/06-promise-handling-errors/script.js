caac7337fbda57a2090720547a9d843af864b202
MZ <nikita.zholkovskyi@gmail.com>
2020-09-08 11:26:09 +0200

    08-asynchronous/06-promise-handling-errors/

--- js-complete-course/08-asynchronous/06-promise-handling-errors/script.js
+++ js-complete-course/08-asynchronous/06-promise-handling-errors/script.js
@@ -10,5 +10,16 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById('run').addEventListener('click', func)
+    function func () {
+        return new Promise((resolve, reject) => {
+            resolve(window.lib.getPersons())
+        }).then(result => {
+            console.log(result);
+        })
+            .catch(function (error) {
+                console.error(error)
+            })
+    }
 })();
+
