0f79a18718bc95f1a867056c003275ddd7def6ff
MZ <nikita.zholkovskyi@gmail.com>
2020-09-08 16:51:55 +0200

    09-fetch/02-list-to-template/script.js 09-fetch/02-list-to-template/script.js
    '
    ''

--- js-complete-course/09-fetch/02-list-to-template/script.js
+++ js-complete-course/09-fetch/02-list-to-template/script.js
@@ -10,5 +10,20 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById('run').addEventListener('click', function  () {
+        fetch('http://localhost:3000/heroes')
+            .then(response => response.json())
+            .then( (data) => appendData(data));
+        function appendData(data) {
+            for (let i = 0; i < data.length; i++) {
+                let temp = document.querySelector('#tpl-hero').content.cloneNode(true)
+                temp.querySelector('.name').innerHTML = data[i].name
+                temp.querySelector('.alter-ego').innerHTML = data[i].alterEgo
+                temp.querySelector('.powers').innerHTML = data[i].abilities
+                target.appendChild(temp)
+
+            }
+        }
+    });
+
 })();
