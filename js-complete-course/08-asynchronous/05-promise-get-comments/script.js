798a3a67242ddaddcd013f94673d799e8cbf52aa
MZ <nikita.zholkovskyi@gmail.com>
2020-09-08 10:04:54 +0200

    08-asynchronous/05-promise-get-comments

--- js-complete-course/08-asynchronous/05-promise-get-comments/script.js
+++ js-complete-course/08-asynchronous/05-promise-get-comments/script.js
@@ -9,6 +9,45 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
+// ***works***
 (() => {
-    // your code here
+
+    document.getElementById('run').addEventListener('click', func)
+
+    function func() {
+        return new Promise((resolve, reject) => {
+            let result = window.lib.getPosts()
+            resolve(result)
+        }).then((result) => result.forEach(function (post) {
+                new Promise((resolve, reject) => {
+                    let result = window.lib.getComments(post.id)
+                    resolve(result)
+                }).then(result => console.log(result))
+
+            }
+        )).catch(function (error) {
+            console.log(error)
+        })
+    }
+
 })();
+
+// *** async version works ***
+// (() => {
+//
+//     document.getElementById('run').addEventListener('click', func)
+//
+//     async function func() {
+//         try {
+//             let resultPost = await window.lib.getPosts()
+//             resultPost.forEach(async function (post) {
+//                 let resultComments = await window.lib.getComments(post.id)
+//                 console.log(resultComments)
+//             })
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     }
+//
+// })();
