314b782d014cde6a98e4a11df1f12df946f2fd08
MZ <nikita.zholkovskyi@gmail.com>
2020-09-09 09:17:46 +0200

    09-fetch/03-details

--- js-complete-course/09-fetch/03-details/script.js
+++ js-complete-course/09-fetch/03-details/script.js
@@ -9,6 +9,26 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
 (() => {
-    // your code here
+    document.getElementById('run').addEventListener('click', function  () {
+        fetch('http://localhost:3000/heroes')
+            .then(response => response.json())
+            .then( (data) => appendData(data));
+        function appendData(data) {
+            for (let i = 0; i < data.length; i++) {
+                let input = document.getElementById('hero-id').value
+                let temp = document.querySelector('#tpl-hero').content.cloneNode(true)
+                if (input == data[i].id) {
+                    document.getElementById('target').innerHTML = ' '
+                    temp.querySelector('.name').innerHTML = data[i].name
+                    temp.querySelector('.alter-ego').innerHTML = data[i].alterEgo
+                    temp.querySelector('.powers').innerHTML = data[i].abilities
+                    target.appendChild(temp)
+                }
+
+            }
+        }
+    });
+
 })();
