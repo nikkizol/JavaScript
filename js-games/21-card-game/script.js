c398aa602c75e7e211f6bb1996aa5c4513e8b6e3
MZ <nikita.zholkovskyi@gmail.com>
2020-09-11 12:08:52 +0200

    added interval on button stay, and every sec gen new number untill it reach 21 or higher than player

--- js-games/21-card-game/script.js
+++ js-games/21-card-game/script.js
@@ -1,4 +1,8 @@
 (() => {
+    let computer = document.getElementById('computer');
+    let player = document.getElementById('player');
+    let totalScore = document.getElementById('totalcompscore');
+    let totalPlayerScore = document.getElementById('totalplayerscore');
     let hitButton = document.getElementById('hit-button');
     let stayButton = document.getElementById('stay-button');
     let shuffleButton = document.getElementById('shuffle');
@@ -9,6 +13,7 @@
     shuffleButton.style.display = 'none';
     resetButton.style.display = 'none';
 
+
     let newGameButton = document.getElementById('new-game-button');
     newGameButton.addEventListener('click', function () {
         newGameButton.style.display = 'none';
@@ -16,24 +21,22 @@
         stayButton.style.display = 'inline';
         shuffleButton.style.display = 'inline';
         resetButton.style.display = 'inline';
-        document.getElementById('computer').innerHTML = "Computer score: " + 0
-        document.getElementById('player').innerHTML = "Player score: " + 0
-        document.getElementById('totalcompscore').innerHTML = 'Total computer: 0'
-        document.getElementById('totalplayerscore').innerHTML = 'Total player: 0'
-    })
+        computer.innerHTML = "Computer score: " + 0
+        player.innerHTML = "Player score: " + 0
+        totalScore.innerHTML = 'Total computer: ' + 0
+        totalPlayerScore.innerHTML = 'Total player: ' + 0
+    });
     shuffleButton.addEventListener('click', function () {
         let computerNumb1 = Math.floor(Math.random() * 10) + 2;
         let computerNumb2 = Math.floor(Math.random() * 10) + 2;
-        // console.log(computerNumb1)
-        // console.log(computerNumb2)
-        document.getElementById('computer').innerHTML = "Computer score: " + computerNumb1 + ", " + computerNumb2
+
+        computer.innerHTML = "Computer score: " + computerNumb1 + ", " + computerNumb2
         let yourSocre1 = Math.floor(Math.random() * 10) + 2;
         let yourSocre2 = Math.floor(Math.random() * 10) + 2;
-        // console.log(yourSocre1)
-        // console.log(yourSocre2)
-        document.getElementById('totalcompscore').innerHTML = 'Total computer: ' + (computerNumb1 + computerNumb2)
-        document.getElementById('totalplayerscore').innerHTML = 'Total player: ' + (yourSocre1 + yourSocre2)
-        document.getElementById('player').innerHTML = "Player score: " + yourSocre1 + ", " + yourSocre2
+
+        totalScore.innerHTML = 'Total computer: ' + (computerNumb1 + computerNumb2);
+        totalPlayerScore.innerHTML = 'Total player: ' + (yourSocre1 + yourSocre2);
+        player.innerHTML = "Player score: " + yourSocre1 + ", " + yourSocre2
         if (yourSocre1 + yourSocre2 == 21) {
             result.innerHTML = 'You Won!!!';
         } else if (yourSocre1 + yourSocre2 > 21) {
@@ -47,70 +50,86 @@
 
         hitButton.addEventListener('click', function () {
             let addNewNumber = Math.floor(Math.random() * 10) + 2;
-            let totalpl = document.getElementById('player').innerHTML += ", " + addNewNumber
-            let totalSplit = totalpl.split(":");
+            player.innerHTML += ", " + addNewNumber
+            let totalSplit = player.innerHTML.split(":");
             let numTotalSplit = totalSplit[1].split(', ')
             let TotalPlayerBe = numTotalSplit.reduce(function (prev, num) {
                 return prev + +num;
             }, 0);
+            let totalSplit1 = computer.innerHTML.split(":");
+            let numTotalSplit1 = totalSplit1[1].split(', ')
+            let TotalCompBe1 = numTotalSplit1.reduce(function (prev, num) {
+                return prev + +num;
+            }, 0);
+            totalPlayerScore.innerHTML = 'Total player: ' + TotalPlayerBe;
             if (TotalPlayerBe == 21) {
                 result.innerHTML = 'You Won!!!';
             } else if (TotalPlayerBe > 21) {
                 result.innerHTML = 'You lost'
+            } else if (TotalPlayerBe == TotalCompBe1 && TotalPlayerBe == 20) {
+                result.innerHTML = 'Its a draw'
+            } else if (TotalCompBe1>21) {
+                result.innerHTML = 'You Won!!!';
+            } else if (TotalPlayerBe>21) {
+                result.innerHTML = 'You lost';
             }
-            document.getElementById('totalplayerscore').innerHTML = 'Total player: ' + TotalPlayerBe
-
+            totalPlayerScore.innerHTML = 'Total player: ' + TotalPlayerBe;
         })
+
         stayButton.addEventListener('click', function () {
-            let totalpc = document.getElementById('computer').innerHTML
-            let totalSplit = totalpc.split(":");
+            let totalCompScore = document.getElementById('totalcompscore');
+            let totalSplit = totalCompScore.innerHTML.split(":");
             let numTotalSplit = totalSplit[1].split(', ')
             let TotalCompBe = numTotalSplit.reduce(function (prev, num) {
+                return prev + +num;
+            }, 0);
+            let totalSplit2 = player.innerHTML.split(":");
+            let numTotalSplit2 = totalSplit2[1].split(', ')
+            let TotalPlayerBe = numTotalSplit2.reduce(function (prev, num) {
                 return prev + +num;
             }, 0);
             if (TotalCompBe == 21) {
                 result.innerHTML = 'You lost'
             } else if (TotalCompBe == 21 && TotalCompBe == 21) {
-                result.innerHTML ='Its a draw'
+                result.innerHTML = 'Its a draw'
+            } else if (TotalCompBe > TotalPlayerBe) {
+                result.innerHTML = 'You lost'
             } else if (TotalCompBe < 20) {
-                let computerNumb3 = Math.floor(Math.random() * 10) + 2;
-                document.getElementById('computer').innerHTML += ", " + (computerNumb3)
+                let intervalId = setInterval(function () {
+                    let randomNumber = Math.round(Math.random() * 10) + 2;
+                    computer.innerHTML += ", " + (randomNumber);
+                    let totalSplit1 = computer.innerHTML.split(":");
+                    let numTotalSplit1 = totalSplit1[1].split(', ')
+                    let TotalCompBe1 = numTotalSplit1.reduce(function (prev, num) {
+                        return prev + +num;
+                    }, 0);
+                    if (TotalCompBe1 > 21) {
+                        result.innerHTML = 'You Won!!!'
+                    }
+                    if (TotalCompBe1 > TotalPlayerBe && TotalCompBe1 <= 21) {
+                        result.innerHTML = 'You Lost'
+                    }
+                    // if (TotalCompBe1 < TotalPlayerBe && TotalPlayerBe <= 21) {
+                    //     result.innerHTML = 'You Won!!!'
+                    // }
+                    if (TotalCompBe1 == 21 && TotalPlayerBe == 21) {
+                        result.innerHTML = 'Its a draw'
+                    }
+                    if (TotalCompBe1 == 21) {
+                        result.innerHTML = 'You Lost'
+                    }
+                    if (TotalCompBe1 > TotalPlayerBe || TotalCompBe1 >= 21) clearInterval(intervalId);
+                    totalCompScore.innerHTML = 'Total computer: ' + TotalCompBe1;
 
-            }
-            let totalcomp = document.getElementById('computer').innerHTML
-            let totalSplit1 = totalcomp.split(":");
-            let numTotalSplit1 = totalSplit1[1].split(', ')
-            let TotalCompBe1 = numTotalSplit1.reduce(function (prev, num) {
-                return prev + +num;
-            }, 0);
-            let totalCompBE = document.getElementById('totalcompscore').innerHTML = 'Total computer: ' + TotalCompBe1
-            let totalcomp1 = document.getElementById('computer').innerHTML
-            let totalSplit2 = totalcomp1.split(":");
-            let numTotalSplit2 = totalSplit2[1].split(', ')
-            let TotalCompBe2 = numTotalSplit2.reduce(function (prev, num) {
-                return prev + +num;
-            }, 0);
-            let totalPlayerBe = document.getElementById('totalplayerscore').innerHTML
-            if (TotalCompBe2 == 21) {
-                result.innerHTML = 'You lost'
-            } else if (TotalCompBe2 > totalPlayerBe ) {
-                result.innerHTML = 'You lost'
-            }else if (TotalCompBe2 < totalPlayerBe ) {
-                result.innerHTML = 'You Won!!!'
-            } else if (TotalCompBe2>21) {
-                result.innerHTML = 'You Won!!!'
+                }, 1000);
+
             }
         })
         resetButton.addEventListener('click', function () {
-            document.getElementById('totalcompscore').innerHTML = 'Total computer: 0'
-            document.getElementById('totalplayerscore').innerHTML = 'Total player: 0'
-            document.getElementById('computer').innerHTML = "Computer score: 0"
-            document.getElementById('player').innerHTML = "Player score: 0"
-            result.innerHTML = ''
-
+            window.location.reload();
         })
-    })
 
+    })
 
 })();
 
